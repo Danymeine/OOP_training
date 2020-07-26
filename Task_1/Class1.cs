3dad11c587652504c56eaa8767dcda4c15d6a8d3
Mister_Meine <mr.daniil1999@list.ru>
2020-07-26 23:59:26 +0300

    1.0

---
+++ Task_1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class A
+    {
+        private int a;
+        private int b;
+        public int c
+
+        {
+            get { return a * b; }
+            set { a = value; }
+        }
+        public A() { a = 6; b = 10; }
+
+    }
+    class B : A
+    {
+        private int d;
+        public int c2
+        {
+            get { return c * ; }
+            set {  = value; }
+        }
+        public B(int m) : base() { c = m }
+        public B() : this() {d = }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Console.WriteLine(a.c);
+            a.c = 10;
+            Console.WriteLine(a.c);
+            B b = new B();
+            Console.WriteLine(a.c);
+            a.c = 10;
+            Console.WriteLine(a.c);
+            Console.ReadLine();
+        }
+    }
+}
